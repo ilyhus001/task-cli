3e0784eff255eab81dddeca7effda9324c5e3615
Ilyaas Hussein <ilyaashussein123@gmail.com>
2024-12-26 11:24:02 -0500

    create file if not exists

---
+++ task_manager.py
@@ -0,0 +1,17 @@
+import os
+import json
+
+FILE = "tasks.json"
+
+def init():
+    if(os.path.exists(FILE)):
+        print("exists")
+    else:
+        with open(FILE, "w") as file:
+            FILE.dump([], file)  # Initialize with an empty list
+
+def main():
+    init()
+
+if __name__ == "__main__":
+    main()
