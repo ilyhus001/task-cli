654b755a0ffc7d4654c892a8dce24cd9527d874b
Ilyaas Hussein <ilyaashussein123@gmail.com>
2024-12-28 01:09:53 -0500

    Implemented addition and deletion of tasks

--- task_manager.py
+++ task_manager.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 import json
 
@@ -7,11 +8,53 @@ def init():
     if(os.path.exists(FILE)):
         print("exists")
     else:
-        with open(FILE, "w") as file:
-            json.dump([], file)  # Initialize with an empty list
+        empty_tasks()  # Initialize with an empty list
+
+def read_json() -> list[dict]:
+   with open(FILE, "r") as file:
+        data = json.load(file)  # Load and parse the JSON file
+        return data 
+
+def write_json(data):
+    with open(FILE, "w") as file:
+        json.dump(data, file, indent=4)
+
+def empty_tasks():
+    with open(FILE, "w") as file:
+        json.dump([], file)
+
+def add(task: str):
+    tasks = read_json()
+    data = {
+      "id": len(tasks) + 1,
+      "description": task, 
+      "status": "todo",
+      "createdAt": datetime.datetime.now().isoformat(),
+      "updatedAt": datetime.datetime.now().isoformat()
+    }
+    tasks.append(data)
+    write_json(tasks)
+
+def delete(id):
+    tasks = read_json()
+    for task in tasks:
+        if task.get('id') == id:
+            tasks.remove(task)
+            update_ids(tasks)
+            break
+    write_json(tasks)
+
+def update_ids(tasks):
+    count = 1
+    for task in tasks:
+        task['id'] = count
+        count+=1
+
 
 def main():
-    init()
+    delete(1)
+    print(read_json())
+   
 
 if __name__ == "__main__":
     main()
